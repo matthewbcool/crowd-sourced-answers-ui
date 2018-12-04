e5b266e012b2371818f0b22ddd6485d2f2bb0210
Matt Cool <matthewbcool@gmail.com>
2018-12-04 14:35:18 -0600

    added up and down votes with numbers

--- components/VotingArrows.js
+++ components/VotingArrows.js
@@ -1,26 +1,42 @@
 import React from "react"
 import { View, StyleSheet, TouchableOpacity, Text } from "react-native"
+import Icon from "react-native-vector-icons/Feather"
 
 const VotingArrows = props => {
+  const upVote = (
+    <Icon name="chevron-up" size={45} color="#900" style={styles.voteArrows} />
+  )
+  const downVote = (
+    <Icon
+      name="chevron-down"
+      size={45}
+      color="#900"
+      style={styles.voteArrows}
+    />
+  )
+  //const AnimatedIcon = Animated.createAnimatedComponent(upVote)
   return (
     <View style={styles.voteArrowContainer}>
-      <TouchableOpacity>
-        <Text>YA</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text>NO</Text>
-      </TouchableOpacity>
+      <TouchableOpacity>{upVote}</TouchableOpacity>
+      <Text>5</Text>
+      <TouchableOpacity>{downVote}</TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   voteArrowContainer: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
     backgroundColor: "#eaeaea",
-    width: 30,
+    width: 40,
     marginBottom: -3,
     marginTop: -3,
     marginRight: 5
+  },
+  voteArrows: {
+    paddingRight: 5
   }
 })
 
